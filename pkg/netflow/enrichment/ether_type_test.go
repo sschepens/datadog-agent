7d2f5de7623c8fb4e0cca068cd694e00ec9db6b2
Alexandre Yang <alexandre.yang@datadoghq.com>
2022-06-01 11:49:16 +0200

    Mapping only ip v4 and v6

--- pkg/netflow/enrichment/ether_type_test.go
+++ pkg/netflow/enrichment/ether_type_test.go
@@ -10,5 +10,4 @@ func TestMapEtherType(t *testing.T) {
 	assert.Equal(t, "", MapEtherType(0x8888))
 	assert.Equal(t, "IPv4", MapEtherType(0x0800))
 	assert.Equal(t, "IPv6", MapEtherType(0x86DD))
-	assert.Equal(t, "ARP", MapEtherType(0x0806))
 }
